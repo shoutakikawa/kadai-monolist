8d339176c3337b6a6e1099e3dd9fa7d00f7b5e9f
shoutakikawa <s.t.214.baystars4@gmail.com>
2017-04-24 01:47:04 +0000

    have-ranking

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
   get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
   
   resources :users, only: [:show, :new, :create]
 
